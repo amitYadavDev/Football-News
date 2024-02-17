a78eff216eaf63665432466c3eb63f8e0e6773e9
amitYadavDev <amit8955461017@gmail.com>
2024-02-17 16:44:37 +0530

    updated

--- src/main/kotlin/DriverClass.kt
+++ src/main/kotlin/DriverClass.kt
@@ -3,19 +3,19 @@ class DriverClass {
 
 fun main() {
 
-    val ricardo = Fan("Ricardo")
-    val maria = Fan("Maria")
+    val amit = Fan("Amit")
+    val raj = Fan("Raj")
 
     NewsSystemBuilder.forTeam(Team.CORINTHIANS)
-        .subscribe(ricardo)
-        .subscribe(maria)
+        .subscribe(amit)
+        .subscribe(raj)
         .notify("Corinthians has won the FIFA World Cup 2 times.")
         .notify("Great news! Roger Guedes scored a hat trick against Palmeiras!")
-        .unsubscribe(ricardo)
+        .unsubscribe(amit)
         .notify("Corinthians will face Flamengo in the COPA DO BRASIL semi-finals.")
 
 
     NewsSystemBuilder.forTeam(Team.LIVERPOOL)
-        .subscribe(ricardo)
+        .subscribe(amit)
         .notify("Luis Diaz is set to return to Liverpool team training this week having been sidelined since October with a knee injury")
 }
